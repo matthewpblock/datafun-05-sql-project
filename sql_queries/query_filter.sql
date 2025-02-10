b74f723a4f30e62fa2ce0045843438fb228eb407
matthewpblock <matthewpblock@gmail.com>
2025-02-09 14:29:34 -1000

    created filter and sorting scripts

--- sql_queries/query_filter.sql
+++ sql_queries/query_filter.sql
@@ -1,1 +1,6 @@
 -- use WHERE to filter data based on conditions.
+
+-- Bench performance
+SELECT *
+FROM player_games
+WHERE start = FALSE;
--- sql_queries/query_sorting.sql
+++ sql_queries/query_sorting.sql
@@ -1,1 +1,8 @@
 -- use ORDER BY to sort data.
+
+-- Rim protection
+SELECT *
+FROM player_games
+ORDER BY blk DESC, dreb DESC;
+
+
