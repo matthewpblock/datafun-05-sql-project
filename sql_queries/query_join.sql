6654ee23c1fd45439bc1e606d27d65eb4d8b2b7b
matthewpblock <matthewpblock@gmail.com>
2025-02-10 20:40:11 -1000

    sorting update

--- sql_queries/query_join.sql
+++ sql_queries/query_join.sql
@@ -2,4 +2,5 @@
 
 SELECT player_games.date, players.first, players.last, player_games.pts, player_games.reb, player_games.ast, player_games.stl, player_games.blk, players.height_inches, players.weight, players.season_exp, players.pos
 FROM player_games
-INNER JOIN players ON players.player_id = player_games.player_id;
+INNER JOIN players ON players.player_id = player_games.player_id
+ORDER BY player_games.reb DESC;
